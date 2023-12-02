1ff9027f90ded9d1e4c3505d94dc859e05ef8fc4
unknown <anabellaqaitru@gmail.com>
2023-12-02 14:20:12 +0100

    unresolved error

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config(); 
+
+
+const app = express();
+const port = 6080;
+
+const mongoUrl = process.env.mongo_Url;
+
+mongoose.connect(mongoUrl)
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('Connected to MongoDB Atlas')
+})
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express with MongoDB!');
+});
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+
+
+app.listen((port), ()=>{
+    console.log(`Listening on port https://localhost${port}`)
+})
